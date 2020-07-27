fa5889576f39fe4f4dd1d64d946f884b981e2b58
Axedez <45712870+Axedez@users.noreply.github.com>
2020-07-27 18:47:56 +0300

    JS (index, music, radio, video players) was added / HTML was changed

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+import {  radioPlayerInit  } from './radioPlayer.js'
+import {  musicPlayerInit  } from './musicPlayer.js'
+import {  videoPlayerInit  } from './videoPlayer.js'
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+const deactivation = () => {
+    temp.style.display = 'none';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+};
+
+
+playerBtn.forEach((button, index) => button.addEventListener('click' , () => {
+        deactivation();
+        button.classList.add('active');
+        playerBlock[index].classList.add('active');
+    }));
+
+
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();
