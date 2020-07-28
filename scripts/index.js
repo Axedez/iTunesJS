44d76c5f1e641eb037ff414a51ab583e460659ad
Axedez <45712870+Axedez@users.noreply.github.com>
2020-07-28 10:47:03 +0300

    Work with videoplayer

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,11 @@ import {  radioPlayerInit  } from './radioPlayer.js'
 import {  musicPlayerInit  } from './musicPlayer.js'
 import {  videoPlayerInit  } from './videoPlayer.js'
 
+
 const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
 const temp = document.querySelector('.temp');
+
 
 const deactivation = () => {
     temp.style.display = 'none';
@@ -14,12 +16,12 @@ const deactivation = () => {
 
 
 playerBtn.forEach((button, index) => button.addEventListener('click' , () => {
-        deactivation();
-        button.classList.add('active');
-        playerBlock[index].classList.add('active');
-    }));
+    deactivation();
+    button.classList.add('active');
+    playerBlock[index].classList.add('active');
+}));
 
-
+console.dir(document);
 radioPlayerInit();
 musicPlayerInit();
 videoPlayerInit();
